b846e1e90dbe98244ea850fc12ad87d9c1b7ba7a
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2026-02-16 12:50:22 +0100

    add task 3

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,10 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while (randomNumber !== x) {
+	randomNumber = Math.round(Math.random() * 16);
+	iteration++;
+}
+
+console.log('Liczba iteracji: ', iteration);
